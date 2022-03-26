bb77ab1187e3aa08eabb71e1b5e1d2fb1fb4f8b5
I. Bugra Aslan <46376529+plaistocene@users.noreply.github.com>
2022-03-26 03:09:40 +0300

    Update Signup.js
    history added to signup, link added to the "already has an account log in"

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
+import { Link, useHistory } from 'react-router-dom'
 import { useAuth } from "../contexts/AuthContext"
 
 
@@ -10,6 +11,7 @@ export default function Signup() {
     const { signup } = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
+    const history = useHistory()
 
     async function handleSubmit(e) {
         e.preventDefault()
@@ -22,6 +24,7 @@ export default function Signup() {
             setError('')
             setLoading(true)
             await signup(emailRef.current.value, passwordRef.current.value)
+            history.push("/")
         } catch {
             setError('Failed to create an account')
         }
@@ -59,7 +62,7 @@ export default function Signup() {
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
-                Already have an account? Log In
+                Already have an account? <Link to="/login">Log In</Link>
             </div>
         </>
     )
