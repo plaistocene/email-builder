9cbe90a9a9d93d2a5d97dbbc9937537b5633ddc8
I. Bugra Aslan <46376529+plaistocene@users.noreply.github.com>
2022-03-26 04:18:14 +0300

    Update Login.js
    added forgot password link

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,7 +15,7 @@ export default function Login() {
     async function handleSubmit(e) {
         e.preventDefault()
 
-         try {
+        try {
             setError('')
             setLoading(true)
             await login(emailRef.current.value, passwordRef.current.value)
@@ -49,6 +49,9 @@ export default function Login() {
                             Log In
                         </Button>
                     </Form>
+                    <div className="w-100 text-center mt-3">
+                        <Link to="/forgot-password">Forgot Password?</Link>
+                    </div>
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
